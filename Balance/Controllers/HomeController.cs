de4cfbbf19c3b05ba80452ee32fe6ec0dec66128
toshisugar <39898922+toshisugar@users.noreply.github.com>
2018-06-20 14:07:27 +0900

    Inputアクション作った

--- Balance/Controllers/HomeController.cs
+++ Balance/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Balance.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,18 @@ namespace Balance.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Input(HomeInputViewModel vm)
+        {
+            return View(vm);
         }
     }
 }
---
+++ Balance/Models/HomeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Balance.Models
+{
+    public class HomeInputViewModel
+    {
+        public string Name { get; set; }
+    }
+}
